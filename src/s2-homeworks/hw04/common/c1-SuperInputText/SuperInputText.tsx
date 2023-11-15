5dfd9f3c58dc8d79cb72305ca98e61c556166fbd
andrejrebrov <andressc@mail.ru>
2023-11-15 14:38:53 +0300

    HW4 created

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -4,8 +4,8 @@ import React, {
     InputHTMLAttributes,
     KeyboardEvent,
     ReactNode,
-} from 'react'
-import s from './SuperInputText.module.css'
+} from "react"
+import s from "./SuperInputText.module.css"
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
@@ -13,7 +13,7 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'type'> & {
+type SuperInputTextPropsType = Omit<DefaultInputPropsType, "type"> & {
     // и + ещё пропсы которых нет в стандартном инпуте
     onChangeText?: (value: string) => void
     onEnter?: () => void
@@ -44,28 +44,33 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
         onKeyPress?.(e)
 
         onEnter && // если есть пропс onEnter
-        e.key === 'Enter' && // и если нажата кнопка Enter
+        e.key === "Enter" && // и если нажата кнопка Enter
         onEnter() // то вызвать его
     }
 
-    const finalSpanClassName = s.error
-        + (spanClassName ? ' ' + spanClassName : '')
-    const finalInputClassName = s.input
-        + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-        + (className ? ' ' + className : '') // задача на смешивание классов
+    /*const finalSpanName = s.error
+    + (spanClassName ? ' ' + spanClassName : '')*/
+
+    const finalSpanClassName = `${error && s.error} ${spanClassName ? spanClassName + spanClassName : ""}`
+
+    /*const finalInputClassName = s.input
+        + (error ? " " + s.errorInput : " " + s.superInput)
+        + (className ? " " + className : "") // задача на смешивание классов*/
+
+    const finalInputClassName = `${error ? s.errorInput : s.superInput} ${className ? className : ""}`
 
     return (
         <div className={s.inputWrapper}>
             <input
                 id={id}
-                type={'text'}
+                type={"text"}
                 onChange={onChangeCallback}
                 onKeyPress={onKeyPressCallback}
                 className={finalInputClassName}
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
             <span
-                id={id ? id + '-span' : undefined}
+                id={id ? id + "-span" : undefined}
                 className={finalSpanClassName}
             >
                 {error}
--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,5 +1,5 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from "react"
+import s from "./SuperButton.module.css"
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
@@ -17,12 +17,15 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    /*const finalClassName = s.button
+        + (disabled
+                ? ...
+                : xType === 'red'
+                    ? ...
+        + (className ? ' ' + className : '') // задачка на смешивание классов*/
+
+
+    const finalClassName = `${s.button} ${disabled ? s.disabled : xType === "red" ? s.red : xType === "secondary" ? s.secondary : s.default} ${className ? className : ""}`
 
     return (
         <button
