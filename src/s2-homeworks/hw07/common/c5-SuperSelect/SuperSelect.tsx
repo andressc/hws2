51f778ba415be3f3bfee91d64245b2435964e29b
andrejrebrov <andressc@mail.ru>
2024-01-18 13:54:31 +0300

    HW 12 Created step 2

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -16,51 +16,39 @@ type SuperSelectPropsType = DefaultSelectPropsType & {
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
-                                                         options,
-                                                         className,
-                                                         onChange,
-                                                         onChangeOption,
-                                                         ...restProps
-                                                     }) => {
+    options,
+    className,
+    onChange,
+    onChangeOption,
+    ...restProps
+}) => {
     const mappedOptions: any[] = options
         ? options.map((o) => (
-            <option
-                id={'hw7-option-' + o.id}
-                className={s.option}
-                key={o.id}
-                value={o.id}
-            >
-                {o.value}
-            </option>
-        ))
+              <option
+                  id={'hw7-option-' + o.id}
+                  className={s.option}
+                  key={o.id}
+                  value={o.id}
+              >
+                  {o.value}
+              </option>
+          ))
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        if (onChangeOption) onChangeOption(Number(e.currentTarget.value))
+        if(onChangeOption) onChangeOption(Number(e.currentTarget.value))
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
 
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "5px",
-            fontFamily: "Montserrat, sans-serif",
-            fontSize: "14px",
-            fontStyle: "normal",
-            fontWeight: 400,
-            lineHeight: "normal"
-        }}>
-            <span>Выберите тему</span>
-            <select
-                className={finalSelectClassName}
-                onChange={onChangeCallback}
-                {...restProps}
-            >
-                {mappedOptions}
-            </select>
-        </div>
+        <select
+            className={finalSelectClassName}
+            onChange={onChangeCallback}
+            {...restProps}
+        >
+            {mappedOptions}
+        </select>
     )
 }
 
